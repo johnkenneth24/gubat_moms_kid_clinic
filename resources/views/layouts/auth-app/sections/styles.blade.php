c7b4d386d81aa2be0af676a590bd86871e0bf722
johnkenneth24 <kengerero@yahoo.com>
2023-08-19 03:24:08 +0800

    added login and signup

---
+++ resources/views/layouts/auth-app/sections/styles.blade.php
@@ -0,0 +1,21 @@
+<!-- BEGIN: Theme CSS-->
+<!-- Fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="{{ asset(mix('assets/vendor/fonts/boxicons.css')) }}" />
+
+<!-- Core CSS -->
+<link rel="stylesheet" href="{{ asset(mix('assets/vendor/css/core.css')) }}" />
+<link rel="stylesheet" href="{{ asset(mix('assets/vendor/css/theme-default.css')) }}" />
+<link rel="stylesheet" href="{{ asset(mix('assets/css/demo.css')) }}" />
+
+<link rel="stylesheet" href="{{ asset(mix('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css')) }}" />
+
+<!-- Vendor Styles -->
+@yield('vendor-style')
+
+
+<!-- Page Styles -->
+@yield('page-style')
