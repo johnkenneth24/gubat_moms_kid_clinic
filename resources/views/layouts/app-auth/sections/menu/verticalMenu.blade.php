19da62dccc1566b4389541c22c7835fd79172070
johnkenneth24 <kengerero@yahoo.com>
2023-10-24 17:03:21 +0800

    sidebar color

--- resources/views/layouts/app-auth/sections/menu/verticalMenu.blade.php
+++ resources/views/layouts/app-auth/sections/menu/verticalMenu.blade.php
@@ -1,9 +1,25 @@
-<aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+@section('page-style')
+    <style>
+        .bg-menu-theme .menu-inner>.menu-item.active:before {
+            background: #ffff !important;
+        }
+
+        .bg-menu-theme .menu-inner>.menu-item.active>.menu-link {
+            background-color: rgb(255 255 255 / 20%) !important;
+        }
+
+        .bg-menu-theme .menu-inner .menu-text-sidebar, .menu-icon{
+            color: #ffff;
+        }
+    </style>
+@endsection
+
+
+<aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme" style="background-color: #696cff !important">
     <!-- ! Hide app brand if navbar-full -->
     <div class="app-brand demo mt-5">
         <a href="{{ route('dashboard') }}" class="app-brand-link">
-            <img src="{{ asset('assets/img/logo.png') }}" height="50" alt="">
-            <span class=" text-primary ml-2 fw-bold">Gubat Mom's & Kids <br> Clinic</span>
+            <h4 class=" text-white ml-2 fw-bold text-uppercase ">Gubat Mom's & Kids Clinic</h4>
         </a>
 
         <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-autod-block d-xl-none" >
@@ -17,7 +33,7 @@
         <li class="menu-item {{ !request()->routeIs('dashboard') ?: 'active' }}">
             <a href="{{ route('dashboard') }}" class="{{-- isset($menu->submenu)?'menu-linkmenu-toggle':'menu-link' --}} menu-link">
                 <i class="menu-icon tf-icons bx bx-heart{{-- $menu->icon --}}"></i>
-                <div>{{-- isset($menu->name)?__($menu->name):'' --}}Dashboard</div>
+                <div class="menu-text-sidebar">{{-- isset($menu->name)?__($menu->name):'' --}}Dashboard</div>
             </a>
         </li>
         {{-- @role('user') --}}
@@ -25,7 +41,7 @@
             <li class="menu-item {{ !request()->routeIs('appointment.*') ?: 'active' }}">
                 <a href="{{ route('appointment.create') }}" class="{{-- isset($menu->submenu)?'menu-linkmenu-toggle':'menu-link' --}} menu-link">
                     <i class="menu-icon tf-icons bx bx-book-bookmark {{-- $menu->icon --}}"></i>
-                    <div>{{-- isset($menu->name)?__($menu->name):'' --}}Book Appointment</div>
+                    <div class="menu-text-sidebar">{{-- isset($menu->name)?__($menu->name):'' --}}Book Appointment</div>
                 </a>
             </li>
 
@@ -33,14 +49,14 @@
                 <a href="{{ route('app-stat.index') }}" class="{{-- isset($menu->submenu)?'menu-linkmenu-toggle':'menu-link' --}} menu-link">
                     <i class="menu-icon tf-icons bx bx-bookmarks{{-- $menu->icon --}}"></i>
                     <i class='bx bx-objects-horizontal-left'></i>
-                    <div>{{-- isset($menu->name)?__($menu->name):'' --}}Appointment Status</div>
+                    <div class="menu-text-sidebar">{{-- isset($menu->name)?__($menu->name):'' --}}Appointment Status</div>
                 </a>
             </li>
 
             <li class="menu-item {{ !request()->routeIs('med-history.*') ?: 'active' }}">
                 <a href="{{ route('med-history.index') }}" class="{{-- isset($menu->submenu)?'menu-linkmenu-toggle':'menu-link' --}} menu-link">
                     <i class="menu-icon tf-icons bx bx-book-content "></i>
-                    <div>{{-- isset($menu->name)?__($menu->name):'' --}}Medical History</div>
+                    <div class="menu-text-sidebar">{{-- isset($menu->name)?__($menu->name):'' --}}Medical History</div>
                 </a>
             </li>
         @endrole
@@ -49,7 +65,7 @@
             <li class="menu-item {{ !request()->routeIs('app-request.*') ?: 'active' }}">
                 <a href="{{ route('app-request.index') }}" class="{{-- isset($menu->submenu)?'menu-linkmenu-toggle':'menu-link' --}} menu-link">
                     <i class="menu-icon tf-icons bx bx-notepad "></i>
-                    <div>{{-- isset($menu->name)?__($menu->name):'' --}}Appointment Request</div>
+                    <div class="menu-text-sidebar">{{-- isset($menu->name)?__($menu->name):'' --}}Appointment Request</div>
                 </a>
             </li>
         @endrole
@@ -58,7 +74,7 @@
             <li class="menu-item {{ !request()->routeIs('app-checkup.*', 'walkin-appointment.*') ?: 'active' }}">
                 <a href="{{ route('app-checkup.index') }}" class="{{-- isset($menu->submenu)?'menu-linkmenu-toggle':'menu-link' --}} menu-link">
                     <i class="menu-icon tf-icons bx bx-calendar "></i>
-                    <div>{{-- isset($menu->name)?__($menu->name):'' --}}Appointment Checkup</div>
+                    <div class="menu-text-sidebar">{{-- isset($menu->name)?__($menu->name):'' --}}Appointment Checkup</div>
                 </a>
             </li>
         @endunlessrole
@@ -67,7 +83,7 @@
             <li class="menu-item {{ !request()->routeIs('patient-record.*') ?: 'active' }}">
                 <a href="{{ route('patient-record.index') }}" class="{{-- isset($menu->submenu)?'menu-linkmenu-toggle':'menu-link' --}} menu-link">
                     <i class="menu-icon tf-icons bx bx-id-card "></i>
-                    <div>{{-- isset($menu->name)?__($menu->name):'' --}}Patient Record History</div>
+                    <div class="menu-text-sidebar">{{-- isset($menu->name)?__($menu->name):'' --}}Patient Record History</div>
                 </a>
             </li>
         @endrole
@@ -77,7 +93,7 @@
             <li class="menu-item {{ !request()->routeIs('user-list.*') ?: 'active' }}">
                 <a href="{{ route('user-list.index') }}" class="{{-- isset($menu->submenu)?'menu-linkmenu-toggle':'menu-link' --}} menu-link">
                     <i class="menu-icon tf-icons bx bx-group "></i>
-                    <div>{{-- isset($menu->name)?__($menu->name):'' --}}Users List</div>
+                    <div class="menu-text-sidebar">{{-- isset($menu->name)?__($menu->name):'' --}}Users List</div>
                 </a>
             </li>
         @endrole
